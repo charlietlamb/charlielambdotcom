102f18e0646d09981907d614afc480f9dc8fb1f4
Charlie Lamb <charlielamb20@icloud.com>
2026-01-24 12:55:57 +0000

    chore: add ferix theme

--- components/section-header.tsx
+++ components/section-header.tsx
@@ -3,7 +3,7 @@ import { Separator } from "./ui/separator";
 export function SectionHeader({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col justify-between gap-1">
-      <h2 className="text-lg text-foreground">{children}</h2>
+      <h2 className="text-lg text-foreground tracking-tight">{children}</h2>
       <Separator />
     </div>
   );
